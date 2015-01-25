395b58b437bb6b6271522fee0f3951e4b0187390
Jose Perez <sinfallas@users.noreply.github.com>
2015-01-25 17:38:16 -0430

    Update calc-mem.sh

--- calc-mem.sh
+++ calc-mem.sh
@@ -7,6 +7,7 @@ page_size=$(getconf PAGE_SIZE)
 phys_pages=$(getconf _PHYS_PAGES)
 shmall=`expr $phys_pages / 2`
 shmmax=`expr $shmall \* $page_size`
+mkdir -p /etc/sysctl.d
 echo kernel.shmmax = $shmmax > /etc/sysctl.d/calc-mem.conf
 echo kernel.shmall = $shmall >> /etc/sysctl.d/calc-mem.conf
 echo kernel.shmmax = $shmmax
