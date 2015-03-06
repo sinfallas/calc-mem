5d560b9ceda07cc7f2bc739b78eaa0d5c357023a
jose perez <sinfallas@gmail.com>
2015-03-05 20:33:05 -0430

    * se agrega color al mensaje final

--- calc-mem.sh
+++ calc-mem.sh
@@ -8,9 +8,9 @@ phys_pages=$(getconf _PHYS_PAGES)
 shmall=`expr $phys_pages / 2`
 shmmax=`expr $shmall \* $page_size`
 mkdir -p /etc/sysctl.d
-echo kernel.shmmax = $shmmax > /etc/sysctl.d/calc-mem.conf
-echo kernel.shmall = $shmall >> /etc/sysctl.d/calc-mem.conf
-echo kernel.shmmax = $shmmax
-echo kernel.shmall = $shmall
-echo "Finished..."
+echo "kernel.shmmax = $shmmax" > /etc/sysctl.d/calc-mem.conf
+echo "kernel.shmall = $shmall" >> /etc/sysctl.d/calc-mem.conf
+echo "kernel.shmmax = $shmmax"
+echo "kernel.shmall = $shmall"
+echo -e "\e[00;1;92mFinished...\e[00m"
 exit 0
